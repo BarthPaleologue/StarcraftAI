6d47de2e88b37dd78cd6803986f19b64ebff4278
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-02 16:59:22 +0100

    added basic 9 pool build order

---
+++ windows/visualstudio/BuildOrder.cpp
@@ -0,0 +1,1 @@
+#include "BuildOrder.h"
---
+++ windows/visualstudio/BuildOrder.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "BWAPI.h"
+#include "Tools.h"
+
+enum OrderItemAction {
+	Build,
+	Cancel
+};
+
+struct OrderItem {
+	int requiredSupply;
+	BWAPI::UnitType unitType;
+	OrderItemAction action;
+
+	std::string toString() {
+		std::stringstream stream;
+		stream << "Required " << requiredSupply << " to " << action << " " << unitType;
+
+		std::string resultString;
+		stream >> resultString;
+
+		return resultString;
+	}
+};
+
+class BuildOrder
+{
+public:
+	BuildOrder() {
+		// see https://liquipedia.net/starcraft/9_Pool_(vs._Terran)
+		_order = {
+			{9, BWAPI::UnitTypes::Zerg_Spawning_Pool, Build},
+			{8, BWAPI::UnitTypes::Zerg_Drone, Build},
+			{9, BWAPI::UnitTypes::Zerg_Extractor, Build},
+			{8, BWAPI::UnitTypes::Zerg_Drone, Build},
+			{9, BWAPI::UnitTypes::Zerg_Extractor, Cancel},
+			{8, BWAPI::UnitTypes::Zerg_Overlord, Build}
+		};
+	}
+
+	/// <summary>
+	/// Checks the current supply against the next stage of the build order. If the supply is enough, the next stage of the build order is executed (return true)
+	/// In the eventuality that the supply is not enough, nothing is done (return false)
+	/// </summary>
+	bool evaluate() {
+		// Returns supply times 2, take divide 2 to get in game supply
+		const int supply = BWAPI::Broodwar->self()->supplyUsed();
+		//std::cout << "Supply: " << (supply / 2) << "\n";
+		const int inGameSupply = supply / 2;
+
+		const OrderItem currentItem = _order[_currentOrderIndex];
+
+		const int currentSupplyThreshold = currentItem.requiredSupply;
+
+		// the supply is not enough, exit the function
+		if (inGameSupply < currentSupplyThreshold) return false;
+
+		switch (currentItem.action) {
+		case Build:
+			Tools::BuildBuilding(currentItem.unitType);
+			break;
+		case Cancel:
+			// how do we cancel the current build of the given item?
+			break;
+		}
+
+		_currentOrderIndex++;
+		return true;
+	}
+
+	void debug() {
+		for (int i = 0; i < _order.size(); i++) {
+			if (i == _currentOrderIndex) {
+				std::cout << ">>>>> ";
+			}
+			else {
+				std::cout << "----- ";
+			}
+
+			std::cout << i << " " << _order[i].toString() << std::endl;
+		}
+	}
+
+private:
+
+	// this is the structure that holds the build order. In a sequence we have the required supply and unit of each stage of the build order
+	std::vector<OrderItem> _order;
+
+	int _currentOrderIndex = 0;
+};
+
