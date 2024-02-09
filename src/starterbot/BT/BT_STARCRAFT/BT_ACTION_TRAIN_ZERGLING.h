63eaacae8c36a0ddb5c0e6caab06e476c483d00f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-09 16:08:22 +0100

    Rework zergling built tree
    - Added a decorator node to know if the build order is finished
    - Refactor train unit node into train zergling node

--- src/starterbot/BT/BT_STARCRAFT/BT_ACTION_TRAIN_ZERGLING.h
+++ src/starterbot/BT/BT_STARCRAFT/BT_ACTION_TRAIN_ZERGLING.h
@@ -4,14 +4,14 @@
 #include <vector>
 #include <BWAPI.h>
 
-class BT_ACTION_TRAIN_UNIT: public BT_ACTION
+class BT_ACTION_TRAIN_ZERGLING: public BT_ACTION
 {
 public:
-    BT_ACTION_TRAIN_UNIT(std::string name, BT_NODE* parent);
+    BT_ACTION_TRAIN_ZERGLING(std::string name, BT_NODE* parent);
 
 private:
     State Evaluate(void* data) override;
     std::string GetDescription() override;
-    static BT_NODE::State TrainUnit(void* data);
+    static BT_NODE::State TrainZergling(void* data);
 };
 
