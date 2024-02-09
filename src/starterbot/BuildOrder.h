06d2de6a507864d3f29d8103f34832ed0462b7c1
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-09 14:48:47 +0100

    added isFinished method to BuildOrder

--- src/starterbot/BuildOrder.h
+++ src/starterbot/BuildOrder.h
@@ -5,7 +5,9 @@
 #include <functional> // to have function as attribute
 
 enum e_orderItemAction {
+	// Build the unit type
 	Build,
+	// Cancel the build process of the unit type
 	Cancel
 };
 
@@ -13,6 +15,8 @@ struct OrderItem {
 	std::function<bool()> conditionForStep; // this int is for humans a readable thing (supply count, building HP, etc)
 	// #TODO: replace this with a job (could be scouting, cancelling, building somewhere, anything)
 	BWAPI::UnitType unitType;
+
+	// The action to perform with the given unitType in the OrderItem struct
 	e_orderItemAction action;
 
 	// #rewrite
@@ -34,6 +38,8 @@ public:
 
 	bool evaluate();
 
+	bool isFinished();
+
 	// #rewrite
 	//void debug() const {
 	//	for (int i = 0; i < m_order.size(); i++) {
@@ -49,8 +55,10 @@ public:
 	//}
 
 private:
-	// this is the structure that holds the build order. In a sequence we have the required supply and unit of each stage of the build order
+	// This array holds the sequence of actions of the build order.
 	std::vector<OrderItem> m_order;
+
+	// the current stage index of the build order
 	int m_currentOrderIndex = 0;
 };
 
