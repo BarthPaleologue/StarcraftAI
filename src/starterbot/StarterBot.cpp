2b78631dfda993d4ecb6920c47268323e3b6193d
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-16 15:54:00 +0100

    set starting threshold supply to 0

--- src/starterbot/StarterBot.cpp
+++ src/starterbot/StarterBot.cpp
@@ -12,7 +12,7 @@ StarterBot::StarterBot()
     pData->currMinerals = 0;
     pData->thresholdMinerals = THRESHOLD1_MINERALS;
     pData->currSupply = 0;
-    pData->thresholdSupply = THRESHOLD1_UNUSED_SUPPLY;
+    pData->thresholdSupply = 0;
 
     pData->nWantedWorkersTotal = NWANTED_WORKERS_TOTAL;
     pData->nWantedWorkersFarmingMinerals = NWANTED_WORKERS_FARMING_MINERALS;
