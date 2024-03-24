2f72009dae62430fdf37f287a2115a59aa2eea77
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-24 20:48:38 +0100

    split all in from main strategy for mutalisks and zerglings

--- src/starterbot/selfmadeBT/MutaliskUtils.h
+++ src/starterbot/selfmadeBT/MutaliskUtils.h
@@ -22,29 +22,40 @@ public:
 
 			auto isAllIn = new BT_DECO_CONDITION_ALL_IN("IsAllIn", chooseStrategy);
 
-			auto allInStrategy = new BT_SEQUENCER("AllInStrategy", isAllIn, 10);
-
-				auto allInIsTargetInvalid = new BT_DECO_CONDITION_SQUAD_INVALID_TARGET("allInIsTargetInvalid", squad, allInStrategy);
-
-					auto allInChooseTarget = new BT_ACTION_MUTALISK_SQUAD_CHOOSE_TARGET("allInChooseTarget", squad, allInIsTargetInvalid);
-
-				auto allInAttack = new BT_ACTION_MUTALISK_SQUAD_ATTACK("AllInAttack", squad, allInStrategy);
-
+			auto allInStrategy = AllInStrategy(squad, isAllIn);
 			
-			auto mainStrategy = new BT_PARALLEL_SEQUENCER("MainStrategy", chooseStrategy, 10);
-
-				auto isTargetInvalid = new BT_DECO_CONDITION_SQUAD_INVALID_TARGET("IsTargetInvalid", squad, mainStrategy);
-
-					auto chooseTarget = new BT_ACTION_MUTALISK_SQUAD_CHOOSE_TARGET("ChooseTarget", squad, isTargetInvalid);
-
-				auto attackActionSelector = new BT_SELECTOR("AttackActionSelector", mainStrategy, 10);
-
-					auto isCoolingDown = new BT_DECO_CONDITION_SQUAD_COOLING_DOWN("IsCoolingDown", squad, attackActionSelector);
-
-						auto fleeAction = new BT_ACTION_MUTALISK_SQUAD_HOLD_POSITION("Flee", squad, isCoolingDown);
-
-					auto attackAction = new BT_ACTION_MUTALISK_SQUAD_ATTACK("Attack", squad, attackActionSelector);
+			auto mainStrategy = MainStrategy(squad, chooseStrategy);
 			
 		return root;
+	}
+
+	static BT_NODE* AllInStrategy(Squad* squad, BT_NODE* parent) {
+		auto allInStrategy = new BT_SEQUENCER("AllInStrategy", parent, 10);
+
+		auto allInIsTargetInvalid = new BT_DECO_CONDITION_SQUAD_INVALID_TARGET("allInIsTargetInvalid", squad, allInStrategy);
+
+		auto allInChooseTarget = new BT_ACTION_MUTALISK_SQUAD_CHOOSE_TARGET("allInChooseTarget", squad, allInIsTargetInvalid);
+
+		auto allInAttack = new BT_ACTION_MUTALISK_SQUAD_ATTACK("AllInAttack", squad, allInStrategy);
+
+		return allInStrategy;
+	}
+
+	static BT_NODE* MainStrategy(Squad* squad, BT_NODE* parent) {
+		auto mainStrategy = new BT_PARALLEL_SEQUENCER("MainStrategy", parent, 10);
+
+		auto isTargetInvalid = new BT_DECO_CONDITION_SQUAD_INVALID_TARGET("IsTargetInvalid", squad, mainStrategy);
+
+		auto chooseTarget = new BT_ACTION_MUTALISK_SQUAD_CHOOSE_TARGET("ChooseTarget", squad, isTargetInvalid);
+
+		auto attackActionSelector = new BT_SELECTOR("AttackActionSelector", mainStrategy, 10);
+
+		auto isCoolingDown = new BT_DECO_CONDITION_SQUAD_COOLING_DOWN("IsCoolingDown", squad, attackActionSelector);
+
+		auto fleeAction = new BT_ACTION_MUTALISK_SQUAD_HOLD_POSITION("Flee", squad, isCoolingDown);
+
+		auto attackAction = new BT_ACTION_MUTALISK_SQUAD_ATTACK("Attack", squad, attackActionSelector);
+
+		return mainStrategy;
 	}
 };
