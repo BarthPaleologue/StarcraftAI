a5d52b391366c56fc9159a7453f6f304c0a670f6
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-24 12:57:35 +0100

    added bt squad deco condition cooldown

---
+++ src/starterbot/BT/BT_STARCRAFT/BT_DECO_CONDITION_SQUAD_COOLING_DOWN.cpp
@@ -0,0 +1,25 @@
+#include "BT_DECO_CONDITION_SQUAD_COOLING_DOWN.h"
+
+BT_DECO_CONDITION_SQUAD_COOLING_DOWN::BT_DECO_CONDITION_SQUAD_COOLING_DOWN(std::string name, Squad squad, BT_NODE* parent)
+	: BT_DECO_SQUAD_CONDITION(name, parent, squad, IsCoolingDown)
+{
+}
+
+std::string BT_DECO_CONDITION_SQUAD_COOLING_DOWN::GetDescription()
+{
+	return "BT_DECO_CONDITION_SQUAD_COOLING_DOWN";
+}
+
+bool BT_DECO_CONDITION_SQUAD_COOLING_DOWN::IsCoolingDown(Squad squad, void* data)
+{
+	// check if all units in the squad are cooling down
+	for (auto unit : squad.getUnits())
+	{
+		if (unit->getAirWeaponCooldown() > 0 || unit->getGroundWeaponCooldown() > 0 || unit->getSpellCooldown() > 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ src/starterbot/BT/BT_STARCRAFT/BT_DECO_CONDITION_SQUAD_COOLING_DOWN.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "BT_DECO_SQUAD_CONDITION.h"
+
+class BT_DECO_CONDITION_SQUAD_COOLING_DOWN
+	: public BT_DECO_SQUAD_CONDITION
+{
+public:
+	BT_DECO_CONDITION_SQUAD_COOLING_DOWN(std::string name, Squad squad, BT_NODE* parent);
+	std::string GetDescription() override;
+
+	static bool IsCoolingDown(Squad squad, void* data);
+};
+
