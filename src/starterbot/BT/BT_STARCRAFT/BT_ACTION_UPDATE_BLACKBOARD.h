82347ddcccd6d0e5f953dd2761d02858c57ed69b
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-24 10:45:54 +0100

    Fixed hazardous template implementation
    - template must be implemented in header files
    - template member functions cannot be virtual

--- src/starterbot/BT/BT_STARCRAFT/BT_ACTION_UPDATE_BLACKBOARD.h
+++ src/starterbot/BT/BT_STARCRAFT/BT_ACTION_UPDATE_BLACKBOARD.h
@@ -4,17 +4,28 @@
 #include <vector>
 #include <BWAPI.h>
 
-//--------------------
 template <typename T>
 class BT_ACTION_UPDATE_BLACKBOARD : public BT_ACTION {
 public:
-    BT_ACTION_UPDATE_BLACKBOARD(std::string name, T& ref, T value, BT_NODE* parent);
+
+    template <typename T>
+    BT_ACTION_UPDATE_BLACKBOARD(std::string name, T& ref, T value, BT_NODE* parent)
+        : BT_ACTION(name, parent), m_value_to_assign(value), m_ref_to_blackboard(ref) {
+    }
 
 private:
     T& m_ref_to_blackboard;
     T m_value_to_assign;
 
-    BT_NODE::State Evaluate(void* data) override;
-    std::string GetDescription() override;
+    template <typename T>
+    BT_NODE::State Evaluate(void* data) {
+        //Blackboard* pData = (Blackboard*)data;
+        m_ref_to_blackboard = m_value_to_assign;
+        return BT_NODE::SUCCESS;
+    }
+
+
+    std::string GetDescription() {
+        return "ACTION_UPDATE_BLACKBOARD";
+    }
 };
-//----------
