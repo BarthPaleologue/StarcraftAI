b58625a8a7dbfba1f355b86c9b15a6d98ae4358f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-24 12:50:48 +0100

    Added BT_DECO_SQUAD_CONDITION

---
+++ src/starterbot/BT/BT_STARCRAFT/BT_DECO_SQUAD_CONDITION.cpp
@@ -0,0 +1,18 @@
+#include "BT_DECO_SQUAD_CONDITION.h"
+
+BT_DECO_SQUAD_CONDITION::BT_DECO_SQUAD_CONDITION(std::string name, BT_NODE* parent, Squad squad, CONDITION_CBK conditionCBK)
+	: BT_DECORATOR(name, parent), m_squad(squad), ConditionCBK(conditionCBK)
+{
+}
+
+
+BT_NODE::State BT_DECO_SQUAD_CONDITION::Evaluate(void* data) {
+    assert(ConditionCBK);
+    if (ConditionCBK(m_squad, data))  return ReturnState(BT_DECORATOR::Evaluate(data));
+    else return Failure();
+}
+
+std::string BT_DECO_SQUAD_CONDITION::GetDescription()
+{
+    return "DECO_SQUAD_CONDITION";
+}
